9d609abea534511b36bcc9cb6d12d52ad4ecb0f6
Bolaji Ogeyingbo <bogeyingbo@gmail.com>
2024-05-12 17:50:09 +0100

    'latest-12-05-2024',

---
+++ src/main/java/com/bogeyingbo/example/springmodel/SpringmodelApplication.java
@@ -0,0 +1,13 @@
+package com.bogeyingbo.example.springmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogeyingbo/example/springmodel/SpringmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogeyingbo.example.springmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
